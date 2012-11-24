e398bb206e293b1bf75b76a58ebc32598b2d02b9
Lee-Jon <leejonball@gmail.com>
2012-11-24 14:20:59 +0000

    Add next three areas of Seabase delta test

--- games/seabase_delta.game.rb
+++ games/seabase_delta.game.rb
@@ -16,21 +16,21 @@ end
 Node.root do
   self.intro = "Seabase delta, classic 1984 game by Firebird software"
   self.help = "Use your 'ed Ed, & always EXAMINE things."
-  
+
   # Start & Station Charlie
   room(:walkway) do
     self.exit_east = :food_farm
     self.desc = <<-DESC
-      I am in a LARGE TUBULAR WALKWAY. Through the transparent walls of the 
+      I am in a LARGE TUBULAR WALKWAY. Through the transparent walls of the
       tube I can see the murky depths of the ocean. The walkway stretches EAST.
     DESC
     self.short_desc = "Large Tubular Walkway."
-    
+
     item(:brief_case, 'briefcase', 'case') do
       self.openable   = true
       self.short_desc = "A briefcase."
       self.presence   = "Briefcase."
-      
+
       item(:documents, 'documents') do
         self.desc = <<-DESC
         oo TOP SECRET TELEX MESSAGE oo
@@ -49,7 +49,7 @@ Node.root do
         self.presence = "Documents"
       end
     end
-    
+
     item(:dead_body, 'body') do
       self.fixed = true
       self.short_desc = "Dead body."
@@ -64,16 +64,16 @@ Node.root do
           return true
         SCRIPT
     end
-    
+
     player
   end
-  
+
   room(:food_farm) do
     self.exit_west  = :walkway
     self.exit_south = :station_charlie
     self.exit_east  = :at_long_table
     self.desc = <<-DESC
-      I've entered the FOOD-FARM AREA. Yuk! The floor is almost completely 
+      I've entered the FOOD-FARM AREA. Yuk! The floor is almost completely
       covered with GREEN SLIMEY SEAWEED! Exits are SOUTH and WEST.
     DESC
     self.short_desc = "Food farm."
@@ -85,7 +85,7 @@ Node.root do
     scenery(:seaweed, 'seaweed') do
     end
   end
-  
+
   room(:at_long_table) do
     self.exit_west = :food_farm
     self.short_desc ="Long Table"
@@ -97,11 +97,11 @@ Node.root do
         puts "What's that on the TABLE?"
         puts "You slide past the table back to the entrance of the room.."
         puts ""
-        
+
         return false
     SCRIPT
   end
-  
+
   room(:station_charlie) do
     self.exit_north = :food_farm
     self.destination = :station_delta
@@ -115,7 +115,7 @@ Node.root do
       puts "You enter the travel-tube car"
       # TODO
     SCRIPT
-    
+
     item(:lever, 'lever') do
       self.presence = "Lever"
       self.script_pull = <<-SCRIPT
@@ -129,10 +129,10 @@ Node.root do
       self.presence = "Rail-tracks"
     end
   end
-  
+
   # The carriage
   room(:carriage) do
-    self.desc = <<-DESC 
+    self.desc = <<-DESC
       I'm in the Travel-Tube car. A metallic voice from the
       loudspeaker-"CLUNK-CLICK OR YOU'RE SURE TO BE SICK"
     DESC
@@ -149,7 +149,7 @@ Node.root do
         puts "Still belted up!"
       end
     SCRIPT
-    
+
     item(:seatbelt, 'belt', 'seatbelt') do
       self.fixed = true
       self.short_desc = 'seat belt.'
@@ -169,7 +169,7 @@ Node.root do
       self.presence = "small slot"
     end
   end
-  
+
   # Around Station Delta
   room(:station_delta) do
     self.exit_north = :observation_dome
@@ -196,7 +196,7 @@ Node.root do
       self.presence = "Huge viewing window"
     end
   end
-  
+
   # Around Station Echo
   room(:station_echo) do
     self.exit_north = :missile_room
@@ -206,7 +206,7 @@ Node.root do
     DESC
     self.short_desc = "Station Echo."
   end
-  
+
   room(:missile_room) do
     self.exit_south = :station_echo
     self.desc = <<-DESC
@@ -222,7 +222,7 @@ Node.root do
     item(:display_screen, 'screen') do
       self.fixed = true
       self.desc = <<-DESC
-        To aim missile enter 1. Degrees Bearing. 2. Elevation. 
+        To aim missile enter 1. Degrees Bearing. 2. Elevation.
         Enter bearing figures now:
       DESC
       self.presence = "Display screen"
@@ -233,6 +233,7 @@ Node.root do
       self.presence = "Small keyboard"
     end
   end
+
   # Around Foxtrot Station & Alpha
   room(:station_foxtrot) do
     self.destination = :station_alpha
@@ -245,7 +246,7 @@ Node.root do
       self.presence = "Screwdriver"
     end
   end
-  
+
   room(:station_alpha) do
     self.destination = :station_beta
     self.desc = <<-DESC
@@ -260,13 +261,128 @@ Node.root do
       self.presence = "Torch"
     end
   end
-  
-  # Station BETA
+
+  # Station BETA plus hatch
   room(:station_beta) do
+    self.exit_south = :corridor1
     self.destination = :station_charlie
+    self.desc = <<-DESC
+      I am on a platform at STATION "BETA". A connecting walk-tube
+      leads off SOUTH.
+    DESC
+    item(:hatch, 'hatch') do
+      self.openable = true
+      self.script_enter = <<-SCRIPT
+        puts "Bang! Hatch slams shut!"
+        get_root.move(:player, :airlock)
+        SCRIPT
+    end
   end
 
-  
+  room(:airlock) do
+    self.desc = "I am in the AIRLOCK."
+    self.short_desc = "Airlock."
+    item(:wheel, 'wheel') do
+      self.presence = "Wheel"
+    end
+    item(:sign, 'sign') do
+      self.presence = "Sign"
+    end
+  end
+
+  room(:corridor1) do
+    self.exit_north = :station_beta
+    self.exit_east  = :corridor2
+    self.exit_west  = :corridor4
+    self.exit_south = :diving_storeroom
+
+    self.desc = <<-DESC
+      I've entered a large CURVING CORRIDOR. It stretches out
+      EAST/WEST & Walkways lead NORTH & SOUTH.
+    DESC
+    self.short_desc = "Corridor"
+  end
+
+  room(:diving_storeroom) do
+    self.exit_north = :corridor1
+    self.desc = <<-DESC
+      I am in the DIVING STOREROOM. A corridor leads NORTH.
+    DESC
+    self.short_desc = "Diving Storeroom"
+
+    item(:cupboard, 'cupboard') do
+      openable = true
+      fixed = true
+      self.presence = "Cupboard"
+
+      item(:diving_suit, 'diving suit') do
+        self.desc = "There's a HOSE fitted."
+        self.presence = "Diving suit"
+        #TODO - when you wear suit it checks for air and says
+        # "Not until you have some AIR matey!"
+      end
+    end
+  end
+
+  room(:corridor2) do
+    self.exit_west = :corridor1
+    self.exit_east = :corridor3
+    self.exit_south = :lift2
+    self.desc = <<-DESC
+      The CURVING EAST/WEST CORRIDOR continues here.
+      There is a simly lit ALCOVE to the SOUTH.
+    DESC
+    item(:sign_missile, 'sign') do
+      self.fixed = true
+      self.desc = <<-DESC
+        In case of ELEVATOR breakdown contact MISS.
+        ISLES on "199"
+      DESC
+    end
+  end
+
+  room(:lift2) do
+    self.exit_north = :corridor2
+    self.desc = <<-DESC
+      Sea-Base Lift Number >2<. The EXIT is to the NORTH.
+    DESC
+    self.short_desc = "Lift 2"
+    item(:floor_panel, 'panel') do
+      self.desc = "It can be OPENED but It's LOCKED!"
+      self.presence = "Floor panel"
+    end
+    item(:lift2_buttons, 'buttons') do
+      self.presence = "Row of buttons"
+    end
+  end
+
+  room(:corridor3) do
+    self.exit_west = :corridor2
+    self.exit_east = :corridor4
+    self.exit_north = :workshop
+
+    self.desc = <<-DESC
+      This CORRIDOR still stretches EAST/WEST. I get the strangest feeling
+      - like I am walking around a huge WHITE MINT! There is also a
+      compartment to the NORTH.
+    DESC
+    self.short_desc = "Corridor"
+  end
+
+  room(:workshop) do
+    self.exit_south = :corridor3
+    self.desc = "This is the ELECTRIC WORKSHOP. The only exit is SOUTH from here."
+    self.short_desc = "Workshop."
+
+    item(:tape_player, 'player') do
+      self.presence = "Tape player."
+      self.short_desc = "Tape player."
+      self.desc = <<-DESC
+        Mitsu-tachi recorder - Insert cassette
+      DESC
+    end
+  end
+
   # Blank room for moving items rather than root
   room(:void) do
     self.desc = "You are in the void - how did you get here?"
@@ -274,7 +390,7 @@ Node.root do
     item(:mover, 'mover', 'metallic') do
       self.presence = "The mover is here."
     end
-    
+
     item(:tube_car, 'car') do
       self.presence = "Travel-Tube car"
       self.script_enter = <<-SCRIPT
@@ -289,7 +405,7 @@ Node.root do
         else
           puts "You look through the pockets and see a card!"
           self.open = true
-          get_root.find(:dead_body).presence = 
+          get_root.find(:dead_body).presence =
             "Dead body - with pockets hanging out"
           get_root.move(:plastic_card, parent)
           return false
@@ -309,7 +425,7 @@ Node.root do
             return false
           elsif args[0].tag != :smallslot
             puts "args not small slot"
-            
+
             return false
           elsif get_room.open
             puts get_room.short_desc
@@ -319,10 +435,10 @@ Node.root do
             puts "The car hurtles along the tunnel at high speed"
             puts " and then jolts to a halt"
             puts "POING! Card pops out."
-            
+
             dest = get_root.find(:tube_car).get_room.destination
             get_root.move(:tube_car, dest, false)
-            
+
             return false
           end
         SCRIPT
