78b52940a6823974c8fd4815af29ab99a232ac11
Lee-Jon <leejonball@gmail.com>
2013-03-06 22:59:25 +0000

    Further refinements to describe.

--- engine.rb
+++ engine.rb
@@ -175,7 +175,7 @@ class Node < OpenStruct
     end
 
     if open && !children.empty?
-      base << "<br> "
+      base << "<br>"
 
       if parent.tag == :root
         base << "You also see:" + "<br>"
@@ -391,22 +391,17 @@ class String
     gsub(/^\s+/, '').
 
     # This one is hard to read.  Replace with any amount
-    # of space after it with that punctuation and two
-    # spaces
-    gsub(/([\.\!\?]+)(\s+)?/, '\1  ').
+    # of space after it with that punctuation and a space
+    gsub(/([\.\!\?\:\,]+)(\s+)?/, '\1 ').
 
-    # Similar to the call above, except replace commas
-    # with a comma and one space
-    gsub(/\,(\s+)?/, ', ').
+    # Now add some paragraphs
+    gsub(/<br>/, "\n").
 
     # The meat of the method, replace between 1 and width
     # characters followed by whitespace or the end of the
     # line with that string and a newline.  This works
     # because regular expression engines are greedy,
     # they'll take as many characters as they can.
-    gsub(%r[(.{1,#{width}})(?:\s|\z)], "\\1\n").
-
-    # Now add some paragraphs
-    gsub(/<br>/, "\n")
+    gsub(%r[(.{1,#{width}})(?:\s|\z)], "\\1\n")
   end
 end
